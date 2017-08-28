9181d0b8d204fb59de36011da31cd8cef6d703c3
sll <sll@slldeimac.lan>
2017-08-28 16:19:15 +0800

    Initial Commit

---
+++ SLL美团HD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SLL美团HD
+//
+//  Created by sll on 2017/8/28.
+//  Copyright © 2017年 sll. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SLL美团HDTests/SLL__HDTests.m
@@ -0,0 +1,39 @@
+//
+//  SLL__HDTests.m
+//  SLL美团HDTests
+//
+//  Created by sll on 2017/8/28.
+//  Copyright © 2017年 sll. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SLL__HDTests : XCTestCase
+
+@end
+
+@implementation SLL__HDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
